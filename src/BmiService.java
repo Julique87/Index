4452bd70beb7f2c7cf0eded92e1a7961e10554cf
Julia Novikova <julikova.n@gmail.com>
2023-02-27 15:41:00 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,6 @@
+public class BmiService {
+    public double calculate(double weight, double tall) {
+        double result = weight / tall;
+        return result;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+    double index = service.calculate(60, 170);
+        System.out.println("Индекс массы тела: " + index);
+    }
+}
