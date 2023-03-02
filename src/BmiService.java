a837c779e88735647497e8b64647014f4aa01d58
Julia Novikova <julikova.n@gmail.com>
2023-03-02 14:51:59 +0300

    Change code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate(double weight, double tall) {
-        double result = weight / tall;
+        double result = weight / Math.pow (tall, 2);
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-    double index = service.calculate(60, 170);
+        double index = service.calculate(60, 1.70);
         System.out.println("Индекс массы тела: " + index);
     }
 }
